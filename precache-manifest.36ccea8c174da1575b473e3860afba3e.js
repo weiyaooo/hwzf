4f4f5d661209ad78019a1b733e27ccb62da82582
weiyaooo <1275865424@qq.com>
2020-06-01 17:04:06 +0800

    Updates

---
+++ precache-manifest.36ccea8c174da1575b473e3860afba3e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1085b9e5092005ed21ce72a00283e9cf",
+    "url": "/hwzf/index.html"
+  },
+  {
+    "revision": "934206aa1b4a61717cc5",
+    "url": "/hwzf/static/css/2.78a87de0.chunk.css"
+  },
+  {
+    "revision": "2a10017a61a956d4269f",
+    "url": "/hwzf/static/css/main.2d873b34.chunk.css"
+  },
+  {
+    "revision": "934206aa1b4a61717cc5",
+    "url": "/hwzf/static/js/2.9a1b55f8.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/hwzf/static/js/2.9a1b55f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a10017a61a956d4269f",
+    "url": "/hwzf/static/js/main.672bad32.chunk.js"
+  },
+  {
+    "revision": "bf2b68c4f9101c2eadc9",
+    "url": "/hwzf/static/js/runtime-main.e54bc26b.js"
+  },
+  {
+    "revision": "3b538f76f922de087df0eb82f703119a",
+    "url": "/hwzf/static/media/iconfont.3b538f76.ttf"
+  },
+  {
+    "revision": "80483e3b898177297ad8b08c1e002f55",
+    "url": "/hwzf/static/media/iconfont.80483e3b.eot"
+  },
+  {
+    "revision": "c931e1ae5f45a521413c9905641f2071",
+    "url": "/hwzf/static/media/iconfont.c931e1ae.svg"
+  },
+  {
+    "revision": "f58b610e9d4e6470d644d7d195bbfda6",
+    "url": "/hwzf/static/media/iconfont.f58b610e.woff"
+  }
+]);
